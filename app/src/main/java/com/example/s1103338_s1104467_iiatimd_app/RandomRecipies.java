3bd07c9f9d8fddcb2401234093391f102fe5c5d8
hantingwu <34519446+hantingwu@users.noreply.github.com>
2020-06-25 16:22:42 +0200

    testen of het werkt

---
+++ app/src/main/java/com/example/s1103338_s1104467_iiatimd_app/RandomRecipies.java
@@ -0,0 +1,16 @@
+package com.example.s1103338_s1104467_iiatimd_app;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RandomRecipies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random);
+    }
+
+}
