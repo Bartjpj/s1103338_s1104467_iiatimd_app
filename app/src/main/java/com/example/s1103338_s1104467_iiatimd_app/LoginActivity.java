627ec1c3bcd85d758f28137ad813529742f92cfc
hantingwu <34519446+hantingwu@users.noreply.github.com>
2020-08-26 13:35:42 +0200

    Login met validatie

--- app/src/main/java/com/example/s1103338_s1104467_iiatimd_app/LoginActivity.java
+++ app/src/main/java/com/example/s1103338_s1104467_iiatimd_app/LoginActivity.java
@@ -3,21 +3,24 @@ package com.example.s1103338_s1104467_iiatimd_app;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.text.Editable;
+import android.text.Layout;
+import android.text.TextWatcher;
 import android.transition.Fade;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText gebruikersnaam, wachtwoord;
+    EditText email, wachtwoord;
     Button btnLogin;
-    Button buttonListView;
+    View layoutEmail, layoutWachtwoord;
+
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -35,17 +38,54 @@ public class LoginActivity extends AppCompatActivity {
         getWindow().setExitTransition(fade);
         setContentView(R.layout.login);
 
-        buttonListView = findViewById(R.id.buttonListView);
-        buttonListView.setOnClickListener(new View.OnClickListener() {
+        btnLogin = findViewById(R.id.buttonListView);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RecyclerRecepies.class);
-                startActivity(intent);
-                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                if (validate()){
+                    Intent intent = new Intent(LoginActivity.this, RecyclerRecepies.class);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+
+                }
+
+            }
+        });
+
+        layoutEmail = findViewById(R.id.layoutEmail);
+        layoutWachtwoord = findViewById(R.id.layoutWachtwoord);
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (email.getText().toString().isEmpty()){
+                    layoutEmail.setErrorEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
 
             }
         });
     }
+    private boolean validate(){
+        if(email.getText().toString().isEmpty()){
+            layoutEmail.setErrorEnabled(true);
+            layoutEmail.setError("Email is verplicht");
+            return false;
+        }
+        if(wachtwoord.getText().toString().isEmpty()){
+            layoutWachtwoord.setError("Wachtwoord is verplicht");
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public void finish(){
         super.finish();
